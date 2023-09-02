e11c6761879cd27392e0795dde0195342e660b5a
FabioHorna <89022118+FabioHorna@users.noreply.github.com>
2023-09-01 19:23:57 -0500

    Add files via upload

---
+++ src/components/BarraSuperior.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="barra-superior">
+      <input type="text" v-model="texto" placeholder="Ingrese su texto aquí" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      texto: ''
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.barra-superior {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  text-align: center;
+  padding: 14px 0;
+}
+
+input[type="text"] {
+  width: 80%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+}
+</style>
